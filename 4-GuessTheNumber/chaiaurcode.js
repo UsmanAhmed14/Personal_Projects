742bd37ad62f9f56ba01775582ce347a53731b09
UsmanAhmed14 <anavidreader14@gmail.com>
2024-03-02 19:44:52 +0500

    impllemented validateGuess function

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -9,7 +9,7 @@ const startOver = document.querySelector(".resultParas");
 
 const p = document.createElement("p");
 
-let prevArray = [];
+let prevGuess = [];
 let numGuesses = 1;
 
 let playGame = true;
@@ -17,7 +17,7 @@ let playGame = true;
 if (playGame) {
   submit.addEventListener("click", function (e) {
     e.preventDefault();
-    const guess = parseInt(userInput.value);
+    const guess = parseInt(userInput.value); //.value property retrieves value inputted by the USER in form
     console.log(guess);
     validateGuess(guess);
   });
@@ -25,6 +25,23 @@ if (playGame) {
 
 function validateGuess(guess) {
   //
+  if (isNaN(guess)) {
+    alert("Please enter a valid number");
+  } else if (guess < 1) {
+    alert("Please enter a number, more than 1");
+  } else if (guess < 100) {
+    alert("Please enter a number, less than 100");
+  } else {
+    prevGuess.push(guess);
+    if (numGuesses === 11) {
+      displayGuess(guess);
+      displayMessage(`Game Over. Random Number was ${randomNumer}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
 }
 
 function checkGuess(guess) {
